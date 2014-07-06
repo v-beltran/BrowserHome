987c9fbb23e0014662e75edc47667db377e8c5bf
v-beltran <bvictor_7@hotmail.com>
2014-07-06 14:25:04 -0400

    Added responsive CSS

--- js/home.js
+++ js/home.js
@@ -1,6 +1,9 @@
 ﻿$(document).ready(function () {
+    // Highlight tile on hover
     $('li').hover(
        function () { $(this).removeClass('opacity') },
        function () { $(this).addClass('opacity') }
 )
+    // Search textbox auto-focus on page load
+    $(".searchText").focus();
 });
